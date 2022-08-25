26a25642c514af618a848e12e822bc261946c6e2
Kate <nemkovich.kate@gmail.com>
2022-08-25 23:09:29 +0300

    Builder

---
+++ Builder/EmailMessage.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp3;
+
+public class EmailMessage
+{
+    private readonly List<string> body = new();
+    private string _from;
+    private string _setSubject;
+    private string _to;
+
+    public EmailMessage From(string from)
+    {
+        _from = from;
+        return this;
+    }
+
+    public EmailMessage To(string to)
+    {
+        _to = to;
+        return this;
+    }
+
+    public EmailMessage SetSubject(string setSubject)
+    {
+        _setSubject = setSubject;
+        return this;
+    }
+
+    public EmailMessage AddBody(string addBody)
+    {
+        body.Add(addBody);
+        return this;
+    }
+
+    public void Send()
+    {
+        Console.WriteLine("От:" + _from);
+        Console.WriteLine("Кому:" + _to);
+        Console.WriteLine("Тема:" + _setSubject);
+        Console.WriteLine("Сообщение:");
+        foreach (var item in body) Console.WriteLine(item);
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp3;
+
+new EmailMessage()
+    .From("Рома")
+    .To("Катя")
+    .SetSubject("Новое сообщение")
+    .AddBody("Давай")
+    .AddBody("Научимся")
+    .AddBody("Строить")
+    .Send();
